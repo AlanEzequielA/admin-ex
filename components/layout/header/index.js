77ad1ce22074e80a96f560ae09baccc84e5f038f
Alan Avila <alan.avila@valtre.onmicrosoft.com>
2022-01-08 12:55:18 -0700

    [Layout] Adds header component

---
+++ components/layout/header/index.js
@@ -0,0 +1,9 @@
+import { Row, Col, Typography } from "antd"
+
+export const Header = () => (
+  <Row justify="center" align="middle">
+    <Col>
+      <Typography.Title level={1}>Admin App</Typography.Title>
+    </Col>
+  </Row>
+)
