e05be88559e26b5b13e4459b83d72ffc868abddb
Alan Avila <alan.avila@valtre.onmicrosoft.com>
2022-01-09 22:40:42 -0700

    [UsersTable] Implements custom table title

--- components/users/table/index.js
+++ components/users/table/index.js
@@ -1,19 +1,37 @@
-import { Button, Space, Table } from "antd";
+import {Button, Space, Table, Popover, Row} from "antd";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons"
+import {TitleTable} from "./title-table";
+import {UserForm} from "../user-form";
 
-export const UsersTable = ({ data, handleEdit, handleDeleteUser }) => (
-  <Table dataSource={data} rowKey={row => row.id}>
+export const UsersTable = ({ data, handleEdit, handleDeleteUser, handleCreate, createForm, editForm }) => (
+  <Table
+    dataSource={data}
+    title={() => (
+      <TitleTable handleCreate={handleCreate} form={createForm}/>
+    )}
+    rowKey={row => row.id}>
     <Table.Column title="Nombre" dataIndex="name"/>
-    <Table.Column title="#Usuario" dataIndex="id"/>
-    <Table.Column title="Numero de telefono" dataIndex="contact"/>
+    <Table.Column title="Número de usuario" dataIndex="id"/>
+    <Table.Column title="Número de telefono" dataIndex="contact"/>
     <Table.Column
       width={50}
       render={ row => (
         <Space direction="horizontal">
-          <Button
-            type="link"
-            icon={<EditOutlined />}
-            onClick={() => handleEdit(row)} />
+          <Popover
+            trigger="click"
+            placement="right"
+            destroyTooltipOnHide={true}
+            content={() => (
+              <UserForm
+                row={row}
+                form={editForm}
+                onSave={handleEdit}
+                title="Editar usuario" />
+            )}>
+            <Button
+              type="link"
+              icon={<EditOutlined />} />
+          </Popover>
           <Button
             danger
             type="link"
