b60fb5c8dc7212369497b576791ce9d87f3f66e3
Alan Avila <alan.avila@valtre.onmicrosoft.com>
2022-01-10 00:31:56 -0700

    [UserTask] Implements functions to create, update and delete tasks

--- components/users/index.js
+++ components/users/index.js
@@ -4,10 +4,10 @@ import {Form} from "antd";
 
 
  const MOCK_USERS = [
-   {name:"epito 0", id:12, contact:"012929292", task:[{ description: "to do", id:"2fd1231" }]},
-   {name:"epito 1", id:13, contact:"012929293", task:[{ description: "to do", id:"23dsf231" }]},
-   {name:"epito 2", id:14, contact:"012929294", task:[{ description: "to do", id:"2312sdf1" }]},
-   {name:"epito 3", id:16, contact:"012929295", task:[{ description: "to do", id:"231qwe31" }]},
+   { name: "Pepito 0", id: 12, contact: "012929292", tasks:[{ description: "Crear repositorio", id: "2fd1231" }, { description: "to do 2", id: "2fddsa1231" }] },
+   { name: "Pepito 1", id: 13, contact: "012929293", tasks:[{ description: "Instalar dependencias", id: "23dsf231" }] },
+   { name: "Pepito 2", id: 14, contact: "012929294", tasks:[{ description: "Crear layout", id: "2312sdf1" }] },
+   { name: "Pepito 3", id: 16, contact: "012929295", tasks:[{ description: "Crear tabla", id: "231qwe31" }] },
  ]
 export const UsersContent = () => {
   const [users, setUsers] = useState(MOCK_USERS)
@@ -39,9 +39,68 @@ export const UsersContent = () => {
     await createForm.validateFields()
     const values = createForm.getFieldsValue()
     setUsers(prev => ([
-      ...prev, { ...values, id: Math.floor((Math.random() * 1000) + 1) }
+      ...prev, { ...values, id: Math.floor((Math.random() * 1000) + 1), tasks: [] }
     ]))
     createForm.resetFields()
+  }
+
+  const handleUpdateTask = ({value, user, setEditTask, item, setIsEditing}) => {
+    if (value === (""||" ")) return
+    setUsers(prev => {
+      const index = users.findIndex(userState => userState.id === user.id)
+      const toUpdateTask = users[index].tasks
+      const newTask = toUpdateTask.map(task =>
+        task.id === item.id ? (
+          {...task, description: value}
+        ) : (
+          task
+        )
+      )
+
+      return prev.map( prevUser =>
+        prevUser.id === user.id ? (
+          {...user, tasks: newTask}
+        ) : (
+          prevUser
+        )
+      )
+    })
+    setIsEditing(false)
+    setEditTask("")
+  }
+
+  const handleDeleteTask = (task, user) => {
+    setUsers(prev => {
+      const index = users.findIndex(userState => userState.id === user.id)
+      const newTask = users[index].tasks.filter(taskToFilter => taskToFilter.id !== task.id)
+
+      return prev.map( prevUser =>
+        prevUser.id === user.id ? (
+          {...user, tasks: newTask}
+        ) : (
+          prevUser
+        )
+      )
+
+    })
+  }
+
+  const handleCreateTask = (task, setState, user) => {
+    if (task === (""||" ")) return
+    setUsers(prev => {
+      const index = users.findIndex(userState => userState.id === user.id)
+      const newTask = users[index].tasks
+      newTask.push({description: task, id: Math.floor((Math.random() * 1000) + 1) })
+
+      return prev.map( prevUser =>
+        prevUser.id === user.id ? (
+          {...user, tasks: newTask}
+        ) : (
+          prevUser
+        )
+      )
+    })
+    setState("")
   }
 
   return (
@@ -51,6 +110,9 @@ export const UsersContent = () => {
       createForm={createForm}
       handleEdit={handleEdit}
       handleCreate={handleCreate}
-      handleDeleteUser={handleDeleteUser} />
+      handleCreateTask={handleCreateTask}
+      handleDeleteUser={handleDeleteUser}
+      handleUpdateTask={handleUpdateTask}
+      handleDeleteTask={handleDeleteTask} />
   )
 }
